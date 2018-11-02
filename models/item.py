f7fcdb2b443da79701aa55e236ec53b3af56dcf0
Kevin Stradtman <stradtkt@gmail.com>
2018-11-02 10:39:18 -0400

    changed some methods in item resource and models

--- models/item.py
+++ models/item.py
@@ -7,6 +7,7 @@ class ItemModel:
 
     def json(self):
         return {'name': self.name, 'price': self.price}
+        
     @classmethod
     def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
@@ -16,7 +17,7 @@ class ItemModel:
         row = result.fetchone()
         connection.close()
         if row:
-            return cls(row[0], row[1])
+            return cls(*row)
 
     def insert(self):
         connection = sqlite3.connect('data.db')
--- resources/item.py
+++ resources/item.py
@@ -10,16 +10,16 @@ class Item(Resource):
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {"message": "Item not found."}
 
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
+        item = ItemModel(name, data['price'])
         try:
-            ItemModel.insert(item)
+            item.insert()
         except:
             return {"message": "An error occurred inserting the item."}, 500
         return item, 201
@@ -36,18 +36,18 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = {'name': name, 'price': data['price']}
+        updated_item = ItemModel(name, data['price'])
         if item is None:
             try:
-                ItemModel.insert(updated_item)
+                updated_item.insert()
             except:
                 return {"message": "An error occurred inserting the item."}
         else:
             try:
-                ItemModel.update(updated_item)
+                updated_item.update()
             except:
                 return {"message": "An error occurred updating the item."}
-        return updated_item
+        return updated_item.json()
 
 class ItemList(Resource):
     def get(self):
