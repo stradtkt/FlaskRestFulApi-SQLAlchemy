feed2c5863f7fc541d6e4b99cc0c8df13e94b3ba
Kevin Stradtman <stradtkt@gmail.com>
2018-11-02 12:57:09 -0400

    added changes to user model and user resource

--- models/user.py
+++ models/user.py
@@ -19,3 +19,11 @@ class UserModel(db.Model):
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- resources/user.py
+++ resources/user.py
@@ -19,10 +19,6 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {'message': 'Username already exists, try again.'}
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
         return {'message': 'User created successfully'}, 201
