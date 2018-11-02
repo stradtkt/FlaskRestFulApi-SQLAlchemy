08849926d930aa941f3dc6b2a0a13684f8c11818
Kevin Stradtman <stradtkt@gmail.com>
2018-11-02 16:50:15 -0400

    added foreign key relationship with store

--- resources/item.py
+++ resources/item.py
@@ -5,6 +5,18 @@ from sercurity import *
 from models.item import ItemModel
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', 
+            type=float,
+            required=True,
+            help="This field cannot be left blank"
+    )
+    parser.add_argument('store_id', 
+            type=int,
+            required=True,
+            help="Every item needs a store_id"
+    )
+
     @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
@@ -16,7 +28,7 @@ class Item(Resource):
         if ItemModel.find_by_name(name):
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
         try:
             item.save_to_db()
         except:
@@ -33,7 +45,7 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
             item.save_to_db()
