fda6f234292fc13b0701a901db55f6a645f79c86
Kevin Stradtman <stradtkt@gmail.com>
2018-11-01 23:30:57 -0400

    added ItemModel

--- resources/item.py
+++ resources/item.py
@@ -3,51 +3,23 @@ from flask import Flask, request
 from flask_restful import Resource, reqparse
 from flask_jwt import JWT, jwt_required
 from sercurity import *
+from models.item import ItemModel
 
 class Item(Resource):
     @jwt_required
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
             return item
         return {"message": "Item not found."}
 
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-        if row:
-            return {"item": {"name": row[0], "price": row[1]}}
-
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-        connection.commit()
-        connection.close()
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-        connection.commit()
-        connection.close()
-
     def post(self, name):
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         try:
-            self.insert(item)
+            ItemModel.insert(item)
         except:
             return {"message": "An error occurred inserting the item."}, 500
         return item, 201
@@ -63,16 +35,16 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         updated_item = {'name': name, 'price': data['price']}
         if item is None:
             try:
-                self.insert(updated_item)
+                ItemModel.insert(updated_item)
             except:
                 return {"message": "An error occurred inserting the item."}
         else:
             try:
-                self.update(updated_item)
+                ItemModel.update(updated_item)
             except:
                 return {"message": "An error occurred updating the item."}
         return updated_item
